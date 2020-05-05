a62b0ad971431bfe0b9af69a93672e6f255ff5f5
Francisco <ainzdono@gmail.com>
2020-05-04 23:36:20 -0400

    Pagina_web

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+		<html lang="es">
+		<head>
+			<meta charset="UTF-8">
+			<title>Blog de Reseñas Francisco</title>
+			<link rel="stylesheet" href="main.css">
+			<link href="https://fonts.googleapis.com/css2?family=Work+Sans&display=swap" rel="stylesheet">
+		</head>
+		<body>
+			
+			<nav>
+				<ul>
+					<li><a href="http://localhost/paginaweb/">Inicio</a></li>
+					
+				</ul>
+			</nav>
+
+			<h1>Blog de Playerunknown's Battlegrounds</h1>
+
+			<div class="contenedor">
+
+				<a href=""><img src="imagen/pubg 4.jpeg" alt=""></a>
+
+			</div>
+			<aside>
+				<div class="redes">
+
+					<h3>Redes Sociales del Juego</h3>
+
+					<a href="https://www.facebook.com/LATAMPUBG/" target="blank">Facebook</a>
+					<a href="https://twitter.com/pubg?lang=es" target="blank">Twitter</a>
+					<a href="https://www.instagram.com/pubg/?hl=es-la" target="blank">Instagram</a>
+				
+				</div>
+			</aside>
+
+			<section>
+			<div class="reseña">
+
+				<h2 class="El mejor Battle Royal"></h2>
+				<p>
+					Un nuevo género del shooter ha venido para quedarse, cada vez lo quieren implementar en más títulos pero no creo que desaparezca cuando la moda pase. PlayerUnknown´s Battleground es una propuesta que ha ido evolucionando con el tiempo y que voy analizar la situación en la que se encuentra ahora desde mi punto de vista.
+
+					Uno de los aspectos más importantes es la jugabilidad en este videojuego se puede decir que es difícil manejar las armas; tienen retroceso y caída. Por eso son tan importantes los accesorios, pueden darte más ventaja. No es lo mismo tener un rifle sin nada que un rifle con cargador ampliado, mira, compensador, empuñadura, etc. En mi caso siempre que tengo un rifle, lo uso en modo semiautomático, es mucho más fácil de controlar y , dependiendo de la situación, un arma automática o a ráfagas para encuentros cercanos, o un franco si las distancias son muy largas. Pero no solo son cruciales las armas, también el equipamiento, tener mochila nos permitirá llevar mas cosas, los cascos y chalecos nos protegerán y las medicinas nos curarán. Hay muchas cosas que coger, por eso durante toda la partida es normal que siempre busquemos e incluso si vamos bien en todo, encontrar un chaleco nivel 3 sorprenderá.
+
+					El juego en equipo se disfruta más, la comunicación es importante para intentar que no muera nadie. Se los digo yo porque no hay nada peor que ser el único al que han tirado y rematado del equipo por ir a tu rushear a lo loco y pasar del resto. Hay que observar, escuchar y comunicar a nuestros compañeros si pasa algo.
+
+					En conclusión, un juego divertido con muchas posibilidades debido a todas las variantes que hay y la mejor opción de entre todos los Battle Royale sin lugar a dudas.
+				</p>
+			</section>
+
+			</div>
+			<footer>
+				<div class="copy">
+					<p>Todos los derechos reservados</p>
+					<p>&copy;Blog de Francisco</p>
+					<p>Contacto: francisco12@gmail.com</p>
+				</div>
+			</footer>
+		</body>
+		</html>		
